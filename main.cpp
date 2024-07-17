a7e0c4667c3ed6d43859df6006ddd4f225c4d5f7
liuzhu <794301938@qq.com>
2024-07-18 00:31:09 +0800

    commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    //测试拷贝构造
+    std::auto_ptr<int> sp1(new int(8));
+    std::auto_ptr<int> sp2(sp1);
+    if (sp1.get() != NULL)
+    {
+        std::cout << "sp1 is not empty." << std::endl;
+    }
+    else
+    {
+        std::cout << "sp1 is empty." << std::endl;
+    }
+
+    if (sp2.get() != NULL)
+    {
+        std::cout << "sp2 is not empty." << std::endl;
+    }
+    else
+    {
+        std::cout << "sp2 is empty." << std::endl;
+    }
+
+    //测试赋值构造
+    std::auto_ptr<int> sp3(new int(8));
+    std::auto_ptr<int> sp4;
+    sp4 = sp3;
+    if (sp3.get() != NULL)
+    {
+        std::cout << "sp3 is not empty." << std::endl;
+    }
+    else
+    {
+        std::cout << "sp3 is empty." << std::endl;
+    }
+
+    if (sp4.get() != NULL)
+    {
+        std::cout << "sp4 is not empty." << std::endl;
+    }
+    else
+    {
+        std::cout << "sp4 is empty." << std::endl;
+    }
+
+    return 0;
+}
